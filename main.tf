41d6191b6a50b296a3a055cc990d2d33c46856a3
skcncuser <52728518+skcncuser@users.noreply.github.com>
2019-07-10 15:00:11 +0900

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,6 @@
+provider "azurerm" {
+    subscription_id = "e0fae348-f6c2-45f5-87b7-c41c22782d8f"
+    client_id       = "6171c1ce-8937-4419-b355-445de75d22be"
+    client_secret   = "XappJxp5/.7rFe1z8zfyspwwTG./0etU"
+    tenant_id       = "96e3cac9-1ab3-436b-9f79-2a0a4b687f1b"
+}
